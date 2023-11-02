17b00559de0146d1ce66b7a8caba28903e27e3c1
Jan Sáblík <j_sablik@utb.cz>
2023-11-02 21:54:53 +0100

    Connected to db, posting Tickets

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,154 @@
+"use client"; // using client-side js
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  const router = useRouter();
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((preState) => ({
+      ...preState, // keep the previous state
+      [name]: value, // update the value of specific key
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevents the page from reloading
+    const res = await fetch("/api/Tickets", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "Content-Type": "application/json",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to create Ticket.");
+    }
+
+    router.refresh();
+    router.push("/");
+  };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create Your Ticket</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows={5}
+        />
+
+        <label>Category</label>
+        <select
+          name="category"
+          onChange={handleChange}
+          value={formData.category}
+        >
+          <option value="Hardware Problem">Hardware problem</option>
+          <option value="Software Problem">Software problem</option>
+          <option value="Project">Project</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            type="radio"
+            id="priority-1"
+            name="priority"
+            value={1}
+            onChange={handleChange}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+          <input
+            type="radio"
+            id="priority-2"
+            name="priority"
+            value={2}
+            onChange={handleChange}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            type="radio"
+            id="priority-3"
+            name="priority"
+            value={3}
+            onChange={handleChange}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            type="radio"
+            id="priority-4"
+            name="priority"
+            value={4}
+            onChange={handleChange}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            type="radio"
+            id="priority-5"
+            name="priority"
+            value={5}
+            onChange={handleChange}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          min={0}
+          max={100}
+          onChange={handleChange}
+          value={formData.progress}
+        />
+        <label>Status</label>
+        <select name="status" onChange={handleChange} value={formData.status}>
+          <option value="not started">Not started</option>
+          <option value="in progress">In progress</option>
+          <option value="completed">Completed</option>
+        </select>
+        <input type="submit" value="Create Ticket" className="btn" />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
---
+++ app/api/Tickets/route.js
@@ -0,0 +1,14 @@
+import Ticket from "@/app/(models)/Ticket";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData); // .create() is built into mongoose
+
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
