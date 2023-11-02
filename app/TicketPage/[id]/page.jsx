7227232ced7e27af43be8e91a2f59108f0199219
Jan Sáblík <j_sablik@utb.cz>
2023-11-02 23:25:53 +0100

    Vercel deployment fix

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,7 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
 const getTicketById = async (id) => {
-  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+  const res = await fetch(`http://127.0.0.1:3000/api/Tickets/${id}`, {
     cache: "no-store",
   });
 
